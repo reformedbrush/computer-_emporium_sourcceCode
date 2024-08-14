2183b7497bcf6b6cb415559be434d7dce1e23c4a
Akshai R Sangeeth <149455586+reformedbrush@users.noreply.github.com>
2024-08-14 13:35:40 +0530

    initial

---
+++ Admin/Admin_reg.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<h1>Admin Registration</h1><br />
+
+<table width="276" border="1">
+  <tr>
+    <td width="169">Name</td>
+    <td width="91"><form id="form2" name="form2" method="post" action="">
+      <label for="admin_name"></label>
+      <input type="text" name="admin_name" id="admin_name" />
+    </form></td>
+  </tr>
+  <tr>
+    <td>E-Mail</td>
+    <td><form id="form3" name="form3" method="post" action="">
+      <label for="txt_email"></label>
+      <input type="text" name="txt_email" id="txt_email" />
+    </form></td>
+  </tr>
+  <tr>
+    <td>Password</td>
+    <td><form id="form4" name="form4" method="post" action="">
+      <label for="txt_password"></label>
+      <input type="text" name="txt_password" id="txt_password" />
+    </form></td>
+  </tr>
+  <tr>
+    <td colspan="2"><form id="form1" name="form1" method="post" action="">
+      <div align="center">
+        <input type="submit" name="submit" id="submit" value="Submit" />
+        <input type="submit" name="Cancel" id="Cancel" value="Cancel" />
+      </div>
+    </form></td>
+  </tr>
+</table>
+</body>
+</html>
---
+++ Admin/shop_reg.php
@@ -0,0 +1,41 @@
+<h1>Shop Registration</h1>
+
+<table width="290" height="327" border="1">
+  <tr>
+    <td width="110">Shop Name</td>
+    <td width="168"><form name="form2" method="post" action="">
+      <label for="shop_name"></label>
+      <input type="text" name="shop_name" id="shop_name">
+    </form></td>
+  </tr>
+  <tr>
+    <td>E-Mail</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Address</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>District</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Logo</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Proof</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Phone No.</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="2"><form name="form1" method="post" action="">
+      <input type="submit" name="SUBMIT" id="SUBMIT" value="Submit">
+      <input type="submit" name="Cancel" id="Cancel" value="Cancel">
+    </form></td>
+  </tr>
+</table>
